f41499267289ebbb8f80b6c518ca7e2dcc35b32e
Trevor Burnham <trevorburnham@gmail.com>
2022-12-17 18:23:11 -0500

    Add minimal styled-components setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,14 @@
+import styled from 'styled-components'
+
+const FlexContainer = styled.div``
+
+const Card = styled.div``
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <FlexContainer>
+      <Card />
+    </FlexContainer>
+  );
 }
